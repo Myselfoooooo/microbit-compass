9ad705aa7d9d7f33791b5c4e79161b7663bcf0cc
Myselfoooooo <105219512+Myselfoooooo@users.noreply.github.com>
2022-05-16 14:50:05 +0200

    I've added sound and the degree you're pointing at.

--- main.ts
+++ main.ts
@@ -6,14 +6,30 @@ basic.forever(function () {
     } else if (bearing < 135 && bearing > 46) {
         basic.showString("W")
         basic.showArrow(ArrowNames.West)
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        basic.pause(2000)
+        basic.clearScreen()
+        basic.showString("" + (bearing))
     } else if (bearing < 225 && bearing > 136) {
         basic.showString("S")
         basic.showArrow(ArrowNames.South)
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        basic.pause(2000)
+        basic.clearScreen()
+        basic.showString("" + (bearing))
     } else if (bearing < 315 && bearing > 226) {
         basic.showString("E")
         basic.showArrow(ArrowNames.East)
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        basic.pause(2000)
+        basic.clearScreen()
+        basic.showString("" + (bearing))
     } else {
         basic.showString("N")
         basic.showArrow(ArrowNames.North)
+        music.playTone(494, music.beat(BeatFraction.Whole))
+        basic.pause(2000)
+        basic.clearScreen()
+        basic.showString("" + (bearing))
     }
 })
